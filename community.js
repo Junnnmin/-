1b58d54963438a231b163909e5da492439134299
Junnnmin <100371785+Junnnmin@users.noreply.github.com>
2025-11-26 14:12:33 +0900

    community function, nonmember search logic
    community function, nonmember search logic edited, profile edited
    firebase 연결이 내 컴퓨터에서 자꾸 안되어서 firebase 기반 개발 부분들은 아직 못한 상태, 
    profile 부분 따로 db에 저장시키기, 커뮤니티 글 작성과 합성, 필요

---
+++ community.js
@@ -0,0 +1,36 @@
+import { db } from "./firebase-config.js";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  query
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const postList = document.getElementById("post-list");
+
+async function loadPosts() {
+  const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
+  const snapshot = await getDocs(q);
+
+  let index = 1;
+
+  snapshot.forEach((doc) => {
+    const data = doc.data();
+    const date = data.createdAt.toDate().toLocaleDateString();
+
+    postList.innerHTML += `
+      <tr class="border-b">
+        <td>${index++}</td>
+        <td>
+          <a href="./post.html?id=${doc.id}" class="text-blue-600 hover:underline">
+            ${data.title}
+          </a>
+        </td>
+        <td>${data.userEmail}</td>
+        <td>${date}</td>
+      </tr>
+    `;
+  });
+}
+
+loadPosts();
---
+++ write.js
@@ -0,0 +1,37 @@
+import { auth, db } from "./firebase-config.js";
+import {
+  addDoc,
+  collection,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const submitBtn = document.getElementById("submit");
+
+let currentUser = null;
+
+// onAuthStateChanged(auth, (user) => {
+//   if (!user) {
+//     alert("로그인이 필요합니다");
+//     location.href = "./login.html";
+//   }
+//   currentUser = user;
+// });
+
+submitBtn.addEventListener("click", async () => {
+  if (!title.value || !content.value) {
+    return alert("제목과 내용을 작성해주세요.");
+  }
+
+  await addDoc(collection(db, "posts"), {
+    title: title.value,
+    content: content.value,
+    // userEmail: currentUser.email,
+    createdAt: serverTimestamp(),
+  });
+
+  alert("글이 작성되었습니다.");
+  location.href = "./community.html";
+});
